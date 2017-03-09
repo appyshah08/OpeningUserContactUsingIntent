93be6deea769e6cc6ff23b6b529aa399ffe78791
appyshah08 <appflamaboy@gmail.com>
2017-03-09 22:48:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dhaval/assignment3b/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.dhaval.assignment3b;
+
+import android.content.Intent;
+import android.drm.DrmStore;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button2);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(ContactsContract.Contacts.CONTENT_URI);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
